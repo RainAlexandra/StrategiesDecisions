7f8cf85d3e8aa1471acfa5f8b1d2cd0b972bd49c
Rain-Alexandra BEGG <rainalex01@gmail.com>
2018-04-11 10:04:59 +0200

    typos in reply and ad strategy interfaces fixed

--- src/strategiesdecisions/IAdvertiseStrategy.java
+++ src/strategiesdecisions/IAdvertiseStrategy.java
@@ -3,8 +3,9 @@ package strategiesdecisions;
 /**
  * <b>SAD</b> - The advertisement strategies depend on whether or not an ad is
  * diffused to all agents or to a subset of agents, whether or not an ad is 
- * diffused only once or many times, and in this case, if it is diffused after
- * a timeout and/or after an event and if the ad is put into a directory or not
+ * diffused only once or many times, and in the latter case, if it is repeated 
+ * after a length of time and/or after an event and if the ad is put into a 
+ * directory or not
  * @author Rain-Alexandra BEGG
  * @version 0.1
  */
--- src/strategiesdecisions/IReplyStrategy.java
+++ src/strategiesdecisions/IReplyStrategy.java
@@ -1,9 +1,9 @@
 package strategiesdecisions;
 
 /**
- * <b>SRP</b> - The Reply strqtegies depend on whether or not the reply is
+ * <b>SRP</b> - The Reply strategies depend on whether or not the reply is
  * immediate or not, if it is to an individual or to a group, non-blocking
- * or not, and if not whether or not replies or memorized or not
+ * or not, and if not whether or not replies are memorized or not
  * @author Rain-Alexandra BEGG
  * @version 0.1
  */

875620647511730ef8b1eee5e43fbb43fa0c7494
Rain-Alexandra BEGG <rainalex01@gmail.com>
2018-04-11 10:18:21 +0200

    NoAd and NoAgree strategies commented

--- src/strategiesdecisions/NoAdvertise.java
+++ src/strategiesdecisions/NoAdvertise.java
@@ -1,10 +1,10 @@
 package strategiesdecisions;
 
 /**
- * @author Rain-Alexandra BEGG
- * @version 0.1
  * <b>SN (SAD)</b> - The agent does not take on any action regardless of 
  * the communication protocol's state/stage
+ * @author Rain-Alexandra BEGG
+ * @version 0.1
  */
 public class NoAdvertise implements IAdvertiseStrategy {
 

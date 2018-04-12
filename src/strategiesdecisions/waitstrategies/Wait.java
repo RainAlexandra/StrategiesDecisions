9661474e124f87ecbcd36404207ee2b7d4bd61cf
Rain-Alexandra BEGG <rainalex01@gmail.com>
2018-04-12 10:47:54 +0200

    first wait strategy (SW1.1) implemented

--- src/strategiesdecisions/waitstrategies/Wait.java
+++ src/strategiesdecisions/waitstrategies/Wait.java
@@ -1,6 +1,7 @@
 package strategiesdecisions.waitstrategies;
 
-import java.util.HashMap;
+import java.util.Comparator;
+import java.util.EnumMap;
 import java.util.TreeSet;
 
 import strategiesdecisions.communication.ICommunication;
@@ -13,12 +14,43 @@ import strategiesdecisions.beans.*;
  * @version 0.1
  */
 public class Wait implements IWaitStrategy {
+	private enum MessageType { AD, AGREEMENT, RESPONSE, SELECTION }
 	
-	HashMap<String, TreeSet<Message>> hm = new HashMap<>();  
+	private class MessageComparator implements Comparator<Message> {
+
+		@Override
+		public int compare(Message msg1, Message msg2) {
+			// maybe sort by interest?
+			int comparison = 0;
+			String contentsMsg1 = msg1.getContents();
+			String contentsMsg2 = msg2.getContents();
+			comparison = contentsMsg1.compareTo(contentsMsg2);
+			return comparison;
+		}
+	}
 	
+	private EnumMap<MessageType, TreeSet<Message>> messages = new EnumMap<>(MessageType.class);  
+	
+	public void setMessages(MessageType msgType, Message msg){
+		TreeSet<Message> msgSet = messages.get(msgType);
+		msgSet.add(msg);
+		this.messages.put(msgType, msgSet);
+	}
+
+	public Wait(){
+		messages.put(MessageType.AD, new TreeSet<>(new MessageComparator()));
+		messages.put(MessageType.AGREEMENT, new TreeSet<>(new MessageComparator()));
+		messages.put(MessageType.RESPONSE, new TreeSet<>(new MessageComparator()));
+		messages.put(MessageType.SELECTION, new TreeSet<>(new MessageComparator()));
+	}
 	
 	@Override
 	public void executer(ICommunication comm){
 		System.out.println("wait");
+		TreeSet<Message> ads = messages.get(MessageType.AD);
+		Message ad = new Ad();
+		ad.setContents("contents");
+		ads.add(ad);
+		messages.put(MessageType.AD, ads);
 	}
 }

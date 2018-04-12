4f6cd3f66a99a08acb33bf62f274b0b1402870b7
Rain-Alexandra BEGG <rainalex01@gmail.com>
2018-04-12 09:36:34 +0200

    communication interface and class comments edited

--- src/strategiesdecisions/communication/Communication.java
+++ src/strategiesdecisions/communication/Communication.java
@@ -5,7 +5,7 @@ import java.util.List;
 import strategiesdecisions.beans.Message;
 
 /**
- * Allows receipt and transmission of messages communicated between agents
+ * Allows the receipt and transmission of messages communicated between agents
  * @author Rain-Alexandra BEGG
  * @version 0.1
  * {@inheritDoc}

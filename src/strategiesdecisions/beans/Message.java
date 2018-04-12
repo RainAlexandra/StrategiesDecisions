423b8118a907bd1c9608a18c1342c6eea40f1a0e
Rain-Alexandra BEGG <rainalex01@gmail.com>
2018-04-12 10:47:30 +0200

    getters and setters added to class

--- src/strategiesdecisions/beans/Message.java
+++ src/strategiesdecisions/beans/Message.java
@@ -6,7 +6,31 @@ package strategiesdecisions.beans;
  * @version 0.1
  */
 public abstract class Message {
-	String emetteur; // refAgent emetteur
-	String destinataire; // refAgent destinataire
-	String contenu;
+	String transmittor; // refAgent transmittor
+	String recipient; // refAgent recipient
+	String contents;
+	
+	public String getTransmittor() {
+		return transmittor;
+	}
+	
+	public String getRecipient() {
+		return recipient;
+	}
+	
+	public String getContents() {
+		return contents;
+	}
+
+	public void setTransmittor(String transmittor) {
+		this.transmittor = transmittor;
+	}
+
+	public void setRecipient(String recipient) {
+		this.recipient = recipient;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
 }

7f538083482b9c93c993bdb6f80d4270605406ee
Rain-Alexandra BEGG <rainalex01@gmail.com>
2018-04-12 09:37:23 +0200

    timeLimitWait comments edited

--- src/strategiesdecisions/waitstrategies/TimeLimitWait.java
+++ src/strategiesdecisions/waitstrategies/TimeLimitWait.java
@@ -3,7 +3,7 @@ package strategiesdecisions.waitstrategies;
 import strategiesdecisions.communication.ICommunication;
 
 /**
- * <b>SW2.2</b> - SN XOR SW1.1 XOR SW1.2 (with a timer "Dt")
+ * <b>SW2.2</b> - SN XOR SW1.1 XOR SW1.2 (with a length of time "Dt")
  * @author Rain-Alexandra BEGG
  * @version 0.1
  */

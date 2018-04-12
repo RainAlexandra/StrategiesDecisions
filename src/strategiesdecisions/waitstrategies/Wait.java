ff1df0fd6d032d32ff943f13cbae80f81e211939
Rain-Alexandra BEGG <rainalex01@gmail.com>
2018-04-12 14:31:58 +0200

    comparator rewritten

--- src/strategiesdecisions/waitstrategies/Wait.java
+++ src/strategiesdecisions/waitstrategies/Wait.java
@@ -1,5 +1,6 @@
 package strategiesdecisions.waitstrategies;
 
+import java.util.Date;
 import java.util.Comparator;
 import java.util.EnumMap;
 import java.util.TreeSet;
@@ -17,25 +18,35 @@ public class Wait implements IWaitStrategy {
 	private enum MessageType { AD, RESPONSE, SELECTION, OTHER }
 	
 	private class MessageComparator implements Comparator<Message> {
-
 		@Override
 		public int compare(Message msg1, Message msg2) {
-			// maybe sort by interest?
-			int comparison = 0;
-			String contentsMsg1 = msg1.getContents();
-			String contentsMsg2 = msg2.getContents();
-			comparison = contentsMsg1.compareTo(contentsMsg2);
-			return comparison;
+			int compDate = 0;
+			Date dateMsg1 = msg1.getDateTime();
+			Date dateMsg2 = msg2.getDateTime();
+			compDate = dateMsg1.compareTo(dateMsg2);
+			if (compDate != 0) return compDate;
+			
+			int compTrans = 0;
+			String transMsg1 = msg1.getTransmitter();
+			String transMsg2 = msg2.getTransmitter();
+			compTrans = transMsg1.compareTo(transMsg2);
+			if (compTrans != 0) return compTrans;
+			
+			int compRec = 0;
+			String recMsg1 = msg1.getRecipient();
+			String recMsg2 = msg2.getRecipient();
+			compTrans = recMsg1.compareTo(recMsg2);
+			if (compRec != 0) return compRec;
+			
+			int compContents = 0;
+			String contMsg1 = msg1.getContents();
+			String contMsg2 = msg2.getContents();
+			compContents = contMsg1.compareTo(contMsg2);			
+			return compContents;
 		}
 	}
 	
-	private EnumMap<MessageType, TreeSet<Message>> messages = new EnumMap<>(MessageType.class);  
-	
-//	public void setMessages(MessageType msgType, Message msg){
-//		TreeSet<Message> msgSet = messages.get(msgType);
-//		msgSet.add(msg);
-//		this.messages.put(msgType, msgSet);
-//	}
+	private EnumMap<MessageType, TreeSet<Message>> messages = new EnumMap<>(MessageType.class);
 	
 	public void setMessages(EnumMap<MessageType, TreeSet<Message>> messages) {
 		this.messages = messages;

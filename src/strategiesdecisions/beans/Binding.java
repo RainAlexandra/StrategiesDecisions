9613def993f5ca77283096065557071a36022820
Rain-Alexandra BEGG <rainalex01@gmail.com>
2018-04-13 10:25:16 +0200

    Binding class added to beans package

---
+++ src/strategiesdecisions/beans/Binding.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package strategiesdecisions.beans;
+
+/**
+ * Binding request type message
+ * @author Rain-Alexandra BEGG
+ * @version 0.1
+ */
+public class Binding extends Message {
+
+	private String service; // refService
+	
+	public Binding(String transmitter, String recipient, String service, String contents, int seqNum) {
+		super(transmitter, recipient, contents, seqNum);
+		this.service = service;
+	}
+
+	public String getService() {
+		return service;
+	}
+
+	public void setService(String service) {
+		this.service = service;
+	}
+}

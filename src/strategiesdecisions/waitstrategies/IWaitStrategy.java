2e8989a9136e772077ea303d51665e44fa38403d
Rain-Alexandra BEGG <rainalex01@gmail.com>
2018-04-13 11:04:20 +0200

    comparator moved to wait interface

--- src/strategiesdecisions/waitstrategies/IWaitStrategy.java
+++ src/strategiesdecisions/waitstrategies/IWaitStrategy.java
@@ -1,5 +1,8 @@
 package strategiesdecisions.waitstrategies;
 
+import java.util.Comparator;
+
+import strategiesdecisions.beans.Message;
 import strategiesdecisions.communication.ICommunication;
 
 /**
@@ -10,5 +13,35 @@ import strategiesdecisions.communication.ICommunication;
  * @version 0.1
  */
 public interface IWaitStrategy {
+	
+	class MessageComparator implements Comparator<Message> {
+		@Override
+		public int compare(Message msg1, Message msg2) {
+			int compSeqNum = 0;
+			Integer seqNumMsg1 = msg1.getSeqNum();
+			Integer seqNumMsg2 = msg2.getSeqNum();
+			compSeqNum = seqNumMsg1.compareTo(seqNumMsg2); // the latest or the oldest???
+			if (compSeqNum != 0) return compSeqNum;
+			
+			int compTrans = 0;
+			String transMsg1 = msg1.getTransmitter();
+			String transMsg2 = msg2.getTransmitter();
+			compTrans = transMsg1.compareTo(transMsg2);
+			if (compTrans != 0) return compTrans;
+			
+			int compRec = 0;
+			String recMsg1 = msg1.getRecipient();
+			String recMsg2 = msg2.getRecipient();
+			compTrans = recMsg1.compareTo(recMsg2);
+			if (compRec != 0) return compRec;
+			
+			int compContents = 0;
+			String contMsg1 = msg1.getContents();
+			String contMsg2 = msg2.getContents();
+			compContents = contMsg1.compareTo(contMsg2);			
+			return compContents;
+		}
+	}
+		
 	public void executer(ICommunication comm);
 }
--- src/strategiesdecisions/waitstrategies/Wait.java
+++ src/strategiesdecisions/waitstrategies/Wait.java
@@ -1,6 +1,5 @@
 package strategiesdecisions.waitstrategies;
 
-import java.util.Comparator;
 import java.util.EnumMap;
 import java.util.Map;
 import java.util.TreeSet;
@@ -20,35 +19,35 @@ public class Wait implements IWaitStrategy {
 	
 //	private enum MessageType { AD, RESPONSE, SELECTION, OTHER }
 	
-	private class MessageComparator implements Comparator<Message> {
-		@Override
-		public int compare(Message msg1, Message msg2) {
-			int compSeqNum = 0;
-			Integer seqNumMsg1 = msg1.getSeqNum();
-			Integer seqNumMsg2 = msg2.getSeqNum();
-			compSeqNum = seqNumMsg1.compareTo(seqNumMsg2); // the latest or the oldest???
-			if (compSeqNum != 0) return compSeqNum;
-			
-			int compTrans = 0;
-			String transMsg1 = msg1.getTransmitter();
-			String transMsg2 = msg2.getTransmitter();
-			compTrans = transMsg1.compareTo(transMsg2);
-			if (compTrans != 0) return compTrans;
-			
-			int compRec = 0;
-			String recMsg1 = msg1.getRecipient();
-			String recMsg2 = msg2.getRecipient();
-			compTrans = recMsg1.compareTo(recMsg2);
-			if (compRec != 0) return compRec;
-			
-			int compContents = 0;
-			String contMsg1 = msg1.getContents();
-			String contMsg2 = msg2.getContents();
-			compContents = contMsg1.compareTo(contMsg2);			
-			return compContents;
-		}
-	}
-	
+//	private class MessageComparator implements Comparator<Message> {
+//		@Override
+//		public int compare(Message msg1, Message msg2) {
+//			int compSeqNum = 0;
+//			Integer seqNumMsg1 = msg1.getSeqNum();
+//			Integer seqNumMsg2 = msg2.getSeqNum();
+//			compSeqNum = seqNumMsg1.compareTo(seqNumMsg2); // the latest or the oldest???
+//			if (compSeqNum != 0) return compSeqNum;
+//			
+//			int compTrans = 0;
+//			String transMsg1 = msg1.getTransmitter();
+//			String transMsg2 = msg2.getTransmitter();
+//			compTrans = transMsg1.compareTo(transMsg2);
+//			if (compTrans != 0) return compTrans;
+//			
+//			int compRec = 0;
+//			String recMsg1 = msg1.getRecipient();
+//			String recMsg2 = msg2.getRecipient();
+//			compTrans = recMsg1.compareTo(recMsg2);
+//			if (compRec != 0) return compRec;
+//			
+//			int compContents = 0;
+//			String contMsg1 = msg1.getContents();
+//			String contMsg2 = msg2.getContents();
+//			compContents = contMsg1.compareTo(contMsg2);			
+//			return compContents;
+//		}
+//	}
+//	
 	private Map<MessageType, TreeSet<Message>> messages = new EnumMap<>(MessageType.class);
 	
 	public void setMessages(EnumMap<MessageType, TreeSet<Message>> messages) {

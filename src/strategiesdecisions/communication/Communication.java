d5ca25bfab2bdd2f6b0a075febab524c54202dec
Rain-Alexandra BEGG <rainalex01@gmail.com>
2018-04-13 22:55:03 +0200

    envoyerMessage prints coded

--- src/strategiesdecisions/communication/Communication.java
+++ src/strategiesdecisions/communication/Communication.java
@@ -2,7 +2,10 @@ package strategiesdecisions.communication;
 
 import java.util.List;
 
+import strategiesdecisions.beans.Binding;
 import strategiesdecisions.beans.Message;
+import strategiesdecisions.beans.MessageType;
+import strategiesdecisions.beans.Selection;
 
 /**
  * Allows the receipt and transmission of messages communicated between agents
@@ -14,16 +17,28 @@ public class Communication implements ICommunication {
 
 	@Override
 	public void diffuserMessage(Message m) {
-		System.out.println("diffuser message par " + m.getTransmitter() + 
-				"\n    " + m.getContents() + "\n    seq = #" + m.getSeqNum());
+		System.out.println("diffuser " + m.getMsgType() + " message par " + m.getTransmitter() + 
+				"\n    " + m.getContents() + "\n    seq = #" + m.getSeqNum() + "\n");
 	}
 
 	@Override
 	// public void envoyerMessage(int emetteur, int destinataire, Message m) {
 	public void envoyerMessage(Message m){
-		System.out.println("envoyer " + m.getMsgType() + " message de " + m.getTransmitter() + " à " + m.getRecipient() + 
-				"\n    " + m.getContents() + "\n    seq = #" + m.getSeqNum());
-		
+		String ret = "";
+		if (m.getMsgType().compareTo(MessageType.BINDING) == 0){
+			ret += "envoyer " + m.getMsgType() + " message de " + m.getTransmitter() + " à " + m.getRecipient() +
+					" pour déclencher le binding physique \n    service = " + ((Binding)m).getService();
+			
+		} else if (m.getMsgType().compareTo(MessageType.SELECTION) == 0){
+			ret += "envoyer " + m.getMsgType() + " message de " + m.getTransmitter() + " à " + m.getRecipient() +
+					" pour déclencher le binding physique \n    binder = " + ((Selection)m).getBinder();
+			
+		} else {
+			ret += "envoyer " + m.getMsgType() + " message de " + m.getTransmitter() + " à " + m.getRecipient();
+				
+		}
+		ret += "\n    " + m.getContents() + "\n    seq = #" + m.getSeqNum() + "\n";
+		System.out.println(ret);
 	}
 
 	@Override

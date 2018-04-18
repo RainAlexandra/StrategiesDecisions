05333662c39c212edb184b5a1088572d746d9262
Rain-Alexandra BEGG <rainalex01@gmail.com>
2018-04-18 10:40:52 +0200

    select strategies implemented

--- src/strategiesdecisions/selectstrategies/DeferredSelect.java
+++ src/strategiesdecisions/selectstrategies/DeferredSelect.java
@@ -3,7 +3,9 @@ package strategiesdecisions.selectstrategies;
 import java.util.LinkedList;
 import java.util.List;
 
+import strategiesdecisions.beans.Binding;
 import strategiesdecisions.beans.Message;
+import strategiesdecisions.beans.Selection;
 import strategiesdecisions.communication.ICommunication;
 import strategiesdecisions.replystrategies.NoReply;
 
@@ -36,10 +38,24 @@ public class DeferredSelect implements ISelectStrategy {
 		System.out.println("deferred-Select");
 		
 		while (dt > 0){
-			(new NoReply()).executer(comm); // on ne fait rien
+			(new NoSelect()).executer(comm); // on ne fait rien
+			// responses <- responses U {r} pour toute reponse r
+			// S <- S - SSL
 			dt--;
 		}
 		
+//		String bestReply = best(replies);
+//		String bestTransmitter = bestReply.getTransmitter();
+		String bestTransmitter = "Y"; // to remove
+		String refBinder = "Binder agent"; // to remove
 		
+//		the binder will be created and initialized with the service of the advertising agent
+		Message binding = new Binding(agent, refBinder, "serviceRef_" + agent, "this is a binding request", 0);
+		Message selection = new Selection(agent, bestTransmitter, "Binder agent", "this is a selection message", 0);
+
+		comm.envoyerMessage(binding);
+		comm.envoyerMessage(selection);
+
+		// S <- SN, SWA
 	}
 }

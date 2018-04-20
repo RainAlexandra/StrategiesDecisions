fe40f086d1cf5168d1e98f81e06fb222d415c2cb
Rain-Alexandra BEGG <rainalex01@gmail.com>
2018-04-20 15:22:16 +0200

    constructor added for broadcast type messages

--- src/strategiesdecisions/beans/Ad.java
+++ src/strategiesdecisions/beans/Ad.java
@@ -7,6 +7,15 @@ package strategiesdecisions.beans;
  */
 public class Ad extends Message {
 
+	
+	/**
+	 * diffusion version, there is no set recipient
+	 */
+	public Ad(String transmitter, String contents, int seqNum){
+		super(transmitter, null, contents, seqNum);
+		this.setMsgType(MessageType.AD);
+	}
+	
 	public Ad(String transmitter, String recipient, String contents, int seqNum){
 		super(transmitter, recipient, contents, seqNum);
 		this.setMsgType(MessageType.AD);
